ff8724df2d9c495eec216ce4007266e12c7b981f
siru <wsr10142@gmail.com>
2023-08-29 16:24:41 +0800

    230829

--- 100/100.c
+++ 100/100.c
@@ -1,8 +1,11 @@
-// 題意思考：
+// 題意思考：輸入n，做迴圈(n為奇數時做n=3n+1;為偶數時做n=n/2;印出n;結束條件為n=1)，印出n印出的次數。
+// 現在要輸入兩個數字，最後印出的結果是這兩個數(包含本身)間總印出次數。
 
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable : 4996)
+
+int n = 0;
 
 int main()
 {
