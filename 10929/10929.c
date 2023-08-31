ce3ed4fb8c1ed326a45477678af3be5a55bc8c0a
Siru <132101788+wsr10142@users.noreply.github.com>
2023-08-31 12:45:58 +0800

    230831

--- 10929/10929.c
+++ 10929/10929.c
@@ -4,51 +4,46 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#pragma warning(sidable : 4996) s
+#pragma warning(sidable : 4996)
+
+//宣告輸入的正整數
+char num[1001]={0};
 
 int main()
 {
-    while (1)
+  //輸入一個正整數判斷是否為11的倍數，直到輸入0結束
+    while (scanf("%s",&num))
     {
-        char n[1001] = {0};
-        int n1[1000] = {0};
-        int s1 = 0;
-        int s2 = 0;
-        int sum = 0;
-        scanf("%s", &n);
-        if (n[0] == '0' && n[1] == '\0')
+        //判斷是否輸入0
+        if (num[0] == '0' && num[1] == '\0')
         {
             break;
         }
-        int d = 1;
-        for (int i = 0; i < strlen(n); i++)
+
+        //宣告偶數位與奇數位的和
+        int sum_even=0;
+        int sum_odd=0;
+      
+        //計算偶數位與奇數位的和
+        for (int i = 0; i < strlen(num); i++)
         {
-            n1[d] = n[i] - '0';
-            d++;
-        }
-        for (int i = 1; i <= strlen(n); i++)
-        {
-            if (i % 2 == 1)
+            if(i%2==0)
             {
-                s1 = s1 + n1[i];
+                sum_odd=sum_odd+(num[i]-'0');
             }
-            else if (i % 2 == 0)
+            else
             {
-                s2 = s2 + n1[i];
+                sum_even=sum_even+(num[i]-'0');
             }
         }
-        if (s1 >= s2)
+        
+        //判斷和之差是否可以整除11
+        if (abs(sum_even-sum_odd) % 11 == 0)
         {
-            sum = s1 - s2;
+            printf("%s is a multiple of 11.\n", num);
         }
         else
-            sum = s2 - s1;
-        if (sum % 11 == 0)
-        {
-            printf("%s is a multiple of 11.\n", n);
-        }
-        else
-            printf("%s is not a multiple of 11.\n", n);
+            printf("%s is not a multiple of 11.\n", num);
     }
 
     system("PAUSE");
