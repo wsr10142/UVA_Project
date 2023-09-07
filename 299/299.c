768a55a094cd0fdae1e5aeab98f522938a5eb470
siru <wsr10142@gmail.com>
2023-09-07 15:55:49 +0800

    230907

--- 299/299.c
+++ 299/299.c
@@ -1,37 +1,50 @@
-// 題意思考：
+// 題意思考：計算交換次數，使用泡沫排序法
 
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable : 4996)
 
+// 宣告測資筆數
+int quantity = o;
+
+// 宣告每筆測資長度
+int length = 0;
+
+// 宣告儲存的數字陣列
+int num[50] = {0};
+
+// 宣告交換次數總和
+int count = 0;
+
 int main()
 {
-    int n = 0, l = 0, s = 0;
-    int num[50] = {0};
-    scanf("%d", &n);
-    while (n != 0)
+    scanf("%d", &quantity);
+    for (int i = 0; i < quantity; i++)
     {
-        scanf("%d", &l);
-        for (int i = 0; i < l; i++)
+        scanf("%d", &length);
+
+        // 將輸入的數字存入陣列
+        for (int j = 0; j < length; j++)
         {
-            scanf("%d", &num[i]);
+            scanf("%d", &num[j]);
         }
-        for (int i = 0; i < 50; i++)
+
+        // 泡沫排序，並計算交換次數
+        for (int m = 0; m < length - 1; m++)
         {
-            for (int j = 0; j < l - 1; j++)
+            for (int n = 0; n < length - m; n++)
             {
-                if (num[j] > num[j + 1])
+                if (num[n] > num[n + 1])
                 {
-                    int flag = num[j + 1];
-                    num[j + 1] = num[j];
-                    num[j] = flag;
-                    s++;
+                    int temp = num[j];
+                    num[j] = num[j + 1];
+                    num[j + 1] = temp;
+                    count++;
                 }
             }
         }
-        printf("Optimal train swapping takes %d swaps.\n", s);
-        s = 0;
-        n--;
+        printf("Optimal train swapping takes %d swaps.\n", count);
+        count = 0;
     }
 
     system("PAUSE");
