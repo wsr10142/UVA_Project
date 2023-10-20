d39c17a729963c452c33050fde1c8d1a641347e7
siru <wsr10142@gmail.com>
2023-10-20 22:19:37 +0800

    231020

---
+++ 10050/10050.c
@@ -0,0 +1,12 @@
+// 題意思考：求到各點的最小距離和，輸入的測資不一定按照大小，先排序縮短執行時間，中位數到各點距離總和會最小
+
+#include <stdio.h>
+#include <stdlib.h>
+#pragma warning(disable : 4996)
+
+int main()
+{
+
+    system("PAUSE");
+    return 0;
+}
