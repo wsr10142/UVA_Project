9356a6d35a63fa3b6c194717fd9d3e3850734ed9
siru <wsr10142@gmail.com>
2023-10-20 23:15:43 +0800

    231020

--- 10931/10931.c
+++ 10931/10931.c
@@ -4,8 +4,17 @@
 #include <stdlib.h>
 #pragma warning(sidable : 4996)
 
+// 宣告輸入的數字
+int num = 0;
+
 int main()
 {
+    scanf("%d", &num);
+
+    // 輸入的數字不為0才處理
+    while (num != 0)
+    {
+    }
 
     system("PAUSE");
     return 0;
