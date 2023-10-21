a843136dad7d01850e72f18b86f55ae4009b1283
Siru <132101788+wsr10142@users.noreply.github.com>
2023-10-21 22:46:39 +0800

    231021

--- 10050/10050.c
+++ 10050/10050.c
@@ -1,11 +1,24 @@
-// 題意思考：求到各點的最小距離和，輸入的測資不一定按照大小，先排序縮短執行時間，中位數到各點距離總和會最小
+// 題意思考：
 
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable : 4996)
 
+int range = 0;
+int political = 0;
+int hartals = 0;
+
 int main()
 {
+    scanf("%d",&range);
+    for(int i = 0; i < range; i++)
+    {
+        scanf("%d", &political);
+        for(int j = 0; j < political; j++)
+        {
+            scanf("")
+        }
+    }
 
     system("PAUSE");
     return 0;
