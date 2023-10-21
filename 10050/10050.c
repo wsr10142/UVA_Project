1447a213fdd3846c1034fc096136be714a82578b
siru <wsr10142@gmail.com>
2023-10-21 23:28:14 +0800

    231021

--- 10050/10050.c
+++ 10050/10050.c
@@ -4,6 +4,7 @@
 #include <stdlib.h>
 #pragma warning(disable : 4996)
 
+int case_num = 0;
 int range = 0;
 int political = 0;
 int political_hartals[100] = {0};
@@ -11,13 +12,13 @@ int hartals = 0;
 
 int main()
 {
-    scanf("%d", &range);
-    for (int i = 0; i < range; i++)
+    for (int i = 0; i < case_num; i++)
     {
+        scanf("%d", &range);
         scanf("%d", &political);
         for (int j = 0; j < political; j++)
         {
-            scanf("")
+            scanf("%d", political_hartals[j]);
         }
     }
 
