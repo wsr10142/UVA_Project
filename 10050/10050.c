0b85e0690fcfd1a673ca3396086083f47620db94
siru <wsr10142@gmail.com>
2023-10-21 22:50:46 +0800

    231021

--- 10050/10050.c
+++ 10050/10050.c
@@ -4,21 +4,8 @@
 #include <stdlib.h>
 #pragma warning(disable : 4996)
 
-int range = 0;
-int political = 0;
-int hartals = 0;
-
 int main()
 {
-    scanf("%d",&range);
-    for(int i = 0; i < range; i++)
-    {
-        scanf("%d", &political);
-        for(int j = 0; j < political; j++)
-        {
-            scanf("")
-        }
-    }
 
     system("PAUSE");
     return 0;
