7880e378dca53839875d94282e872b8c3c561fa2
siru <wsr10142@gmail.com>
2023-10-22 02:54:09 +0800

    231022

--- 10050/10050.c
+++ 10050/10050.c
@@ -12,14 +12,34 @@ int hartals = 0;
 
 int main()
 {
+    scanf("%d", &case_num);
+
     for (int i = 0; i < case_num; i++)
     {
         scanf("%d", &range);
         scanf("%d", &political);
+
         for (int j = 0; j < political; j++)
         {
-            scanf("%d", political_hartals[j]);
+            scanf("%d", &political_hartals[j]);
         }
+
+        for (int k = 1; k < range + 1; k++)
+        {
+            if (k % 7 != 6 && k % 7 != 0)
+            {
+                for (int m = 0; m < political; m++)
+                {
+                    if (k % political_hartals[m] == 0)
+                    {
+                        hartals++;
+                        break;
+                    }
+                }
+            }
+        }
+
+        printf("%d\n", hartals);
     }
 
     system("PAUSE");
