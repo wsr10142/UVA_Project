2c5dd3576a79fe43f9bda1ebc5c9a8f6522c4d47
Siru <132101788+wsr10142@users.noreply.github.com>
2023-10-22 16:47:08 +0800

    231022

---
+++ 10056/10056.c
@@ -0,0 +1,34 @@
+// 題意思考：給定測資數量、天數、政黨數量、政黨罷工頻率，判斷政黨總罷工天數，星期五跟六不算
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#pragma warning(disable : 4996)
+
+// 宣告case數量
+int case_num = 0;
+
+// 宣告玩家數量
+int player = 0;
+
+// 宣告贏的機率
+double probability = 0;
+
+// 宣告第幾位獲勝
+int win_ith = 0;
+
+int main()
+{
+    scanf("%d", &case_num);
+
+    for (int i = 0; i < case_num; i++)
+    {
+        scanf("%d %f %d", &player, &probability, win_ith);
+
+        // 印出結果
+        printf("%4f\n", );
+    }
+
+    system("PAUSE");
+    return 0;
+}
