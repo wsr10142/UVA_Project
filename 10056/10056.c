9a4e1e977a0becb0e8ac7c89a5939cc482d5162e
siru <wsr10142@gmail.com>
2023-10-22 23:26:53 +0800

    231022

--- 10056/10056.c
+++ 10056/10056.c
@@ -24,10 +24,10 @@ int main()
 
     for (int i = 0; i < case_num; i++)
     {
-        scanf("%d %f %d", &player, &probability, win_ith);
+        scanf("%d %lf %d", &player, &probability, &win_ith);
 
         // 勝利機率為0
-        if (p == 0)
+        if (probability == 0)
         {
             printf("0.0000\n");
         }
@@ -37,9 +37,17 @@ int main()
         {
             // 失敗的機率
             double q = 1 - probability;
-            // q^[(r-1)*n] * q^(i-1) * p
+
+            // 第一回合贏(表示I前面的人全輸)：q^(I-1) * p
+            // 第二回合贏(表示第一輪大家全輸)：q^N * q^(I-1) * p
+            // 第三回合贏(表示前兩輪大家全輸)：q^2N * q^(I-1) * p
+            // .....
+            // 第X+1回合贏(表示前X輪大家全輸)：q^XN * q^(I-1) * p
+            // 因此，計算第I個人贏的機率為將所有可能相加
+            // 利用等比級數和公式：首項為 q^(I-1) * p，公比為 q^N
+            // 化簡為：q^(I-1) * p/(1-q^N)
             // 印出結果
-            printf("%.4f\n", pow());
+            printf("%.4f\n", pow(q, (win_ith - 1)) * probability / (1 - pow(q, player)));
         }
     }
 
