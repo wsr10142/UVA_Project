c97175727835ecff08e5e0b31a1632fa92bbf7fe
siru <wsr10142@gmail.com>
2023-10-25 01:43:50 +0800

    231025

--- 10170/10170.c
+++ 10170/10170.c
@@ -1,26 +1,19 @@
-// 題意思考：給定速度及經過時間，計算經過兩倍時間的位移為何
+// 題意思考：
 
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable : 4996)
 
-// 宣告速度
-int speed = 0;
+// 宣告第一組入住的旅團人數
+int size = 0;
 
-// 宣告經過的時間
-int time = 0;
-
-// 宣告位移
-int area = 0;
+// 宣告題目詢問第幾天時(飯店有幾人)
+int day_ith = 0;
 
 int main()
 {
-    while (scanf("%d %d", &speed, &time) != EOF)
+    while (scanf("%d %d", &size, &day_ith) != EOF)
     {
-        // 位移為速度乘上時間
-        area = speed * (2 * time);
-
-        printf("%d\n", area);
     }
 
     system("PAUSE");
