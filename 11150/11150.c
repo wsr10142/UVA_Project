3caf0c028366f8c8f922184f925a27739111195a
Siru <132101788+wsr10142@users.noreply.github.com>
2023-10-31 15:31:26 +0800

    231031

--- 11150/11150.c
+++ 11150/11150.c
@@ -1,4 +1,5 @@
-// 題意思考：給定cola初始數量，每三個空瓶可以再換一瓶cola，最多可以喝幾瓶。
+// 題意思考：給定cola初始數量，每三個空瓶可以再換一瓶cola，
+//         可以先跟別人借空瓶，但最後要可以歸還，最多可以喝幾瓶。
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,17 +8,11 @@
 // 宣告cola初始數量
 int cola = 0;
 
-// 可借的瓶子
-int empty = 0;
-
-// 總共可以喝到的瓶數
-int total = 0;
-
 int main()
 {
     while (scanf("%d", &cola) != EOF)
     {
-        printf("%d\n",cola*3/2);
+        printf("%d\n", cola * 3 / 2);
     }
 
     system("PAUSE");
