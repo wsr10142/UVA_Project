1f87f68f45137ac06a3994fea3eb2ce361530081
Siru <132101788+wsr10142@users.noreply.github.com>
2023-10-31 13:26:23 +0800

    231031

--- 11150/11150.c
+++ 11150/11150.c
@@ -1,17 +1,13 @@
-// 題意思考：給定速度及經過時間，計算經過兩倍時間的位移為何
+// 題意思考：給定cola初始數量，每三個空瓶可以再換一瓶cola，最多可以喝幾瓶。
 
 #include <stdio.h>
 #include <stdlib.h>
 #pragma warning(disable : 4996)
 
-// 宣告速度
-int speed = 0;
+// 宣告cola初始數量
+int cola = 0;
 
-// 宣告經過的時間
-int time = 0;
-
-// 宣告位移
-int area = 0;
+int ;
 
 int main()
 {
