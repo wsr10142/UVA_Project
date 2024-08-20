7b0e3eddfbdc151757a48a5a53db74d8eb968729
Siru <wsr10142@gmail.com>
2024-08-20 15:21:18 +0800

    240820

--- 10167/10167.c
+++ 10167/10167.c
@@ -7,18 +7,26 @@
 // 櫻桃數量
 int size = 0;
 
-// 放置座標位置，動態配置陣列大小
-int pos_x = 0;
-int pos_y = 0;
-
 int main()
 {
+    scanf("%d",&size);
+
+    // 放置座標位置，配置陣列大小
+    int pos_x[size] = 0;
+    int pos_y[size] = 0;
+
     // 輸入位置並存入array
-    for(int i = 0; i < size; i++)
+    for(int i = 0; i < 2 * size; i++)
     {
         scanf("%d",&pos_x[i]);
         scanf("%d",&pos_y[i]);
     }
+
+    for(int i = 0;i < 2 * size; i++)
+    {
+        printf("%d %d\n",pos_x[i],pos_y[i]);
+    }
+
 
     system("PAUSE");
     return 0;
