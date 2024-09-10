62b9d73ef0345fa78a25d968cee52c40d646bc54
Siru <wsr10142@gmail.com>
2024-09-10 13:13:27 +0800

    240910

--- 10190/10190.c
+++ 10190/10190.c
@@ -4,28 +4,10 @@
 #include <stdlib.h>
 #pragma warning(disable : 4996)
 
-// 宣告第一組入住的旅團人數
-int size = 0;
-
-// 宣告題目詢問第幾天時(飯店有幾人)
-long long int day_ith = 0;
 
 int main()
 {
-    while (scanf("%d %lld", &size, &day_ith) != EOF)
-    {
 
-        long long int sum = size;
-
-        // 利用等差級數和求出特定日期的旅團人數
-        while (sum < day_ith)
-        {
-            size = size + 1;
-            sum = sum + size;
-        }
-
-        printf("%d\n", size);
-    }
 
     system("PAUSE");
     return 0;
